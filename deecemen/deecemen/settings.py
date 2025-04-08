5b84053607a9b86a52da3a214bf1ad3c2683367f
RazaTurabi <razaturabi92@gmail.com>
2025-04-08 13:44:44 +0530

    up

--- deecemen/deecemen/settings.py
+++ deecemen/deecemen/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6-qhtr*_i#)jgqawg)eb6+dss63(*+7)56kvv3k^8vat((2u1#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
